4e4bab907f5d0e390689f176742d13d832d9fa91
rkirk430 <r.kirk430@gmail.com>
2022-05-19 21:31:09 -0400

    OK

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const app = express();
 
 //Step 4: Create Middleware
 app.use(cors());
-app.use(express.json()); // parse json bodies
+// app.use(express.json()); // parse json bodies
 
 
 app.get("/", (req, res) => {
     res.send("hello world");  //Renders hello world
 });
 
-//People index Route//
+//People index Route//////
 app.get("/stocks", async (req, res) => {
     try {
         const response = await fetch("https://apewisdom.io/api/v1.0/filter/all-stocks/page/4");
